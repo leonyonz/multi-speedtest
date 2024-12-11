54812d5145e6823e75097a85405efdb06c7ea49c
leonyonz <42370275+leonyonz@users.noreply.github.com>
2024-12-11 09:44:16 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -76,11 +76,12 @@ speed_test() {
 speed() {
     speed_test '' 'Speedtest.net'
     speed_test '2233'  'Telkom SBY    ID'
-    speed_test '8579'  'Biznet  ID'
+    speed_test '8689'  'Biznet Jimbaran ID'
+    speed_test '38128' 'Biznet SBY  ID'
     speed_test '5935' 'My Republic  SG'
     speed_test '4802' 'FirstMedia  ID'
     speed_test '12807' 'CBN JKT  ID'
-    speed_test '16375' 'CN Mobile   CN'
+    speed_test '15248' 'Toronto   CA'
     speed_test '3242'  'Netherland   NL'
     speed_test '14623' 'Japan      JP'
 }
