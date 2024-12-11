a148b6de6a6f335864debe46db9418a42cd79bca
leonyonz <42370275+leonyonz@users.noreply.github.com>
2024-12-11 09:56:44 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -74,7 +74,7 @@ speed_test() {
 }
 
 speed() {
-    speed_test '' 'Speedtest.net'
+    speed_test '13362' 'Speedtest.net'
     speed_test '1372'  'Telkom SBY    ID'
     speed_test '8689'  'Biznet Jimbaran ID'
     speed_test '38128' 'Biznet SBY  ID'
